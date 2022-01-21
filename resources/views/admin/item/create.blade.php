68af5f92a4d731c34d0f400260ae2879c5ca606b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-01-21 13:26:47 +0900

    商品入力フォーム追加

--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -1,1 +1,8 @@
 <h2>商品入力</h2>
+<a href="{{ route('admin.item.index') }}">商品一覧</a>
+
+<form action="{{ route('admin.item.add') }}" method="post">
+  <label for="">商品名</label>
+  <input type="text" name="name">
+  <button>更新</button>
+</form>
