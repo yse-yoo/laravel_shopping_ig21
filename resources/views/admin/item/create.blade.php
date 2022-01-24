9dc40c4aad191ba17a872969bf6c7c1288165149
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-01-24 14:00:45 +0900

    入力フォーム修正

--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -1,10 +1,21 @@
-<h2>商品入力</h2>
-<a href="{{ route('admin.item.index') }}">商品一覧</a>
+<h2>{{ __('Item Input') }}</h2>
+<a href="{{ route('admin.item.index') }}">{{ __('Back') }}</a>
 
 <form action="{{ route('admin.item.add') }}" method="post">
   @csrf
 
-  <label for="">商品名</label>
-  <input type="text" name="name">
-  <button>更新</button>
+  <div>
+    <label for="">{{ __('Item Name') }}</label>
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="">{{ __('Item Code') }}</label>
+    <input type="text" name="code">
+  </div>
+  <div>
+    <label for="">{{ __('Price') }}</label>
+    <input type="number" name="price">
+  </div>
+
+  <button>{{ __('Update') }}</button>
 </form>
